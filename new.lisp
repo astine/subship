abae68ce71e6d37dcf941335aaf30337e957f328
illuminati <illuminati@beggersandbuskers.com>
2010-01-17 12:47:28 -0500

    modified:   new.lisp
    	another restructuring of the code

--- new.lisp
+++ new.lisp
@@ -1,7 +1,6 @@
 ;subship game
 
-
-;;utility macros
+;;utility macros/functions
 (defmacro aif (test true-case &body false-case)
   "Anaphoric If"
   `(let ((it ,test))
@@ -14,6 +13,57 @@
 	 (and it (aand ,@(rest forms))))
       (car forms)))
 
+(defun make-keyword (string)
+  "Converts string into keyword"
+  (intern (string-upcase string) :keyword))
+
+(defun random-element (list)
+  "Selects a random element from a list"
+  (nth (random (list-length list)) list))
+
+(defmacro setf/incf (place delta)
+  "Increment place by delta. If place is nil, treat it as zero"
+  `(if ,place
+       (incf ,place ,delta)
+       (setf ,place ,delta)))
+
+(defmacro defmemoized (name args &body body)
+  "Defines a memoized function"
+  `(let ((cache (make-hash-table :test #'equal)))
+     (defun ,name ,args
+       (or (gethash (list ,@args) cache)
+	   (let ((output (progn ,@body)))
+	     (setf (gethash (list ,@args) cache) output)
+	     output)))))
+
+(defun greater (x y &optional (test #'>))
+  "Returns the greater of two alternatives as defined by test."
+  (if (funcall test x y) x y))
+
+(defun greatest (list &optional (test #'>))
+  "Returns the greatest of a list of alternatives as defined by test."
+  (reduce (lambda (x y) 
+	    (greater x y test))
+	  list))
+
+(defun average (list)
+  (/ (apply #'+ list) (list-length list)))
+
+(defun shuffle (sequence)
+  "destructively shuffles a sequence"
+  (labels ((swap (seq i j)
+	     (let ((temp (elt seq i)))
+	       (setf (elt seq i) (elt seq j))
+	       (setf (elt seq j) temp))))
+    (dotimes (index (length sequence))
+      (swap sequence index (+ (random (- (length sequence) index)) index)))))
+
+(defun shuffle-list (list)
+  "Non-destructively shuffles a list"
+  (let ((vector (map 'vector #'identity list)))
+    (shuffle vector)
+    (map 'list #'identity vector)))
+
 ;;Code to deal with directions and vectors
 
 ;directions are represented with keywords
@@ -21,17 +71,22 @@
 ;y is up and down (north and south)
 ;up and right are positive, left and down are negative
 
-(defconstant +directions+ (let ((table (make-hash-table :size 8 :rehash-threshold 0)))
-			    (setf (gethash :north table) '(0 . 1))
-			    (setf (gethash :south table) '(0 . -1))
-			    (setf (gethash :east table) '(1 . 0))
-			    (setf (gethash :west table) '(-1 . 0))
-			    (setf (gethash :northeast table) '(1 . 1))
-			    (setf (gethash :northwest table) '(-1 . 1))
-			    (setf (gethash :southeast table) '(1 . -1))
-			    (setf (gethash :southwest table) '(-1 . -1))
-			    table)
-  "directions represented as vectors, listed under their respective keywords")
+(defun get-directions ()
+  '(:north :south :east :west :northeast :northwest :southeast :southwest))
+
+(defun move-location (location direction)
+  "Returns the location in direction of 'direction' from 'location'"
+  (case direction
+    (:north (cons (car location) (1+ (cdr location))))
+    (:south (cons (car location) (1- (cdr location))))
+    (:east (cons (1+ (car location)) (cdr location)))
+    (:west (cons (1- (car location)) (cdr location)))
+    (:northeast (cons (1+ (car location)) (1+ (cdr location))))
+    (:northwest (cons (1- (car location)) (1+ (cdr location))))
+    (:southeast (cons (1+ (car location)) (1- (cdr location))))
+    (:southwest (cons (1- (car location)) (1- (cdr location))))
+    (otherwise (progn (warn "Bad Direction: ~A~%" direction)
+		      location))))
 
 (defun in-vector (start direction target)
   "Returns true if target is direction from start."
@@ -55,9 +110,11 @@
 		     (< (car start) (car target))))
     (:southwest (and (= (- (car start) (car target))
 			(- (cdr start) (cdr target)))
-		     (> (car start) (car target))))))
+		     (> (car start) (car target))))
+    (otherwise (progn (warn "Bad Direction ~A~%" direction)
+		      nil))))
 
-;;ship definitions
+;; defining the ship class basic operations
 (defclass ship ()
   ((x-coordinate :accessor x-coordinate
 		 :initarg :x-coordinate
@@ -68,140 +125,130 @@
    (direction :accessor direction
 	      :initarg :direction
 	      :documentation "The direction; can be :north, :south, :east, :west, :northeast, :northwest, :southeast, :southwest.")
+   (alive :accessor alive
+	  :initarg :alive
+	  :initform t
+	  :documentation "boolean signifier of whether ship is still in game")))
+
+(defgeneric location (ship))
+
+(defmethod location ((ship ship))
+  "Returns the location of a ship as pair"
+  (cons (x-coordinate ship) (y-coordinate ship)))
+
+(defmethod location ((ship cons))
+  ship)
+
+(defun alive? (ship)
+  (when (alive ship) ship))
+
+;; defining the remembered-ship class and operations
+(defclass remembered-ship (ship)
+  ((age :accessor age
+	:initarg :age
+	:initform 0
+	:documentation "Turns since last seen")
+   (original :accessor original
+	     :initarg :original
+	     :documentation "Reference to the ship being remembered")))
+
+(defmethod name ((ship remembered-ship))
+  (name (original ship)))
+
+(defun create-remembered-ship (ship)
+  "Creates a remembered-ship from a ship"
+  (make-instance 'remembered-ship 
+		 :x-coordinate (x-coordinate ship)
+		 :y-coordinate (y-coordinate ship)
+		 :direction (direction ship)
+		 :alive (alive ship)
+		 :age 0
+		 :original ship))
+
+(defun refresh-remembered-ship (ship)
+  "Refresh a remembered-ship"
+  (setf (x-coordinate ship) (x-coordinate (original ship)))
+  (setf (y-coordinate ship) (y-coordinate (original ship)))
+  (setf (direction ship) (direction (original ship)))
+  (setf (alive ship) (alive (original ship)))
+  (setf (age ship) 0))
+
+(defun age-remembered-ship (ship)
+  "Age the memory of a remembered-ship by a turn"
+  (incf (age ship)))
+
+(defclass ship-with-memory (ship)
+  ((name :accessor name
+	 :initarg :name
+	 :initform "no name"
+	 :documentation "Identitfying name for ship")
    (memory :accessor memory
 	   :initarg :memory
-	   :initform nil
+	   :initform nil 
 	   :documentation "Memory of recent views of ships.")))
 
-(defun create-ship (location direction)
+(defun create-ship (name location direction &optional type)
   "Returns a brand new ship object"
-  (unless (gethash direction +directions+)
-    (error "bad direction: ~A~%" direction))
-  (make-instance 'ship 
+  (make-instance type
+		 :name name
 		 :x-coordinate (car location)
 		 :y-coordinate (cdr location)
 		 :direction direction))
 
-(defun location (ship)
-  (cons (x-coordinate ship) (y-coordinate ship)))
+(defun remember-ship (remembering-ship remembered-ship)
+  (setf (getf (memory remembering-ship) (name remembered-ship))
+	(create-remembered-ship remembered-ship)))
 
-(defclass memory ()
-  ((ships :accessor ships
-	  :initarg :ships
-	  :initform (make-hash-table :test #'equal)
-	  :documentation "Memories of ships")
-   (ages :accessor ages
-	 :initarg :ages
-	 :initform (make-hash-table :test #'equal)
-	 :documentation "Age of each memory.")))
+(defun refresh-memory-by-name (remembering-ship name)
+  (aif (getf (memory remembering-ship) name)
+       (refresh-remembered-ship it)
+       (warn "Bad Name: ~A~%" name)))
 
-(defun create-memory (initial-ship-states)
-  (let ((memory (make-instance 'memory)))
-    (maphash (lambda (name ship)
-	       (setf (gethash name (ships memory)) 
-		     (create-ship (location ship) (direction ship)))
-	       (setf (gethash name (ages memory)) 0))
-	     initial-ship-states)
-    memory))
+(defun map-ship-memory (remembering-ship function)
+  (loop for ship in (rest (memory remembering-ship)) by #'cddr
+     collect (funcall function ship)))
 
-(defgeneric forget-ship (name forgetter))
+(defun refresh-memory (remembering-ship)
+  (map-ship-memory remembering-ship #'refresh-remembered-ship))
+	 
+(defun age-memory (remembering-ship)
+  (map-ship-memory remembering-ship #'age-remembered-ship))
 
-(defmethod forget-ship (name (forgetter memory))
-  (remhash name (ships forgetter))
-  (remhash name (ages forgetter)))
+;; Managing ships for the game
+(defvar *all-ships* nil "List for all playing ships.")
 
-(defmethod forget-ship (name (forgetter ship))
-  (forget-ship name (memory forgetter)))
+(defun add-ship (name location direction &optional type)
+  "Adds a ship to the game"
+  (let ((new-ship (create-ship name location direction type)))
+    (dolist (ship *all-ships*)
+      (remember-ship ship new-ship)
+      (remember-ship new-ship ship))
+    (push new-ship *all-ships*)))
 
-(defun initialize-memories (initial-ship-states)
-  (maphash (lambda (name ship)
-	     (setf (memory ship) (create-memory initial-ship-states))
-	     (forget-ship name ship))
-	   initial-ship-states))
+(defun get-ship-by-name (name)
+  "Returns ship by name"
+  (dolist (ship *all-ships*)
+    (when (equal (name ship) name)
+      (return-from get-ship-by-name ship))))
 
-(defun age (memory)
-  (maphash (lambda (name age)
-	     (incf (gethash name (ages memory))))
-	   (ages memory)))
+(defun move (ship direction)
+  (let ((new-location (move-location (location ship) direction)))
+    (setf (x-coordinate ship) (car new-location))
+    (setf (y-coordinate ship) (cdr new-location))
+    (setf (direction ship) direction)))
 
-(defvar *all-ships* (make-hash-table :test #'equal)
-  "Global hash of all ships.")
+(defun peek (peeking-ship)
+  (refresh-memory peeking-ship)
+  (dolist (ship (remove peeking-ship *all-ships*))
+    (refresh-memory-by-name ship (name peeking-ship))))
 
-(defun add-ship (name location direction)
-  "Adds a ship to the global hash"
-  (when (gethash name *all-ships*)
-    (error "Ship ~A, already exists" name))
-  (setf (gethash name *all-ships*) 
-	(create-ship location direction)))
-
-(defun getship (name)
-  (gethash name *all-ships*))
-
-(defun remove-ship (name)
-  (let ((removable-ship (gethash name *all-ships*)))
-    (remhash name *all-ships*)
-    (maphash (lambda (ship-name ship)
-	       (forget-ship ship-name removable-ship))
-	     *all-ships*)))
-
-
-(defgeneric remember (name ship rememberer))
-
-(defmethod remember (name (ship ship) (rememberer memory))
-  (if ship
-      (progn
-	(setf (gethash name (ships rememberer))
-	      (create-ship (location ship) (direction ship)))
-	(setf (gethash name (ages rememberer)) 0))
-      (remhash name (ships rememberer))))
-
-(defmethod remember (name ship (rememberer ship))
-  (remember name ship (memory rememberer)))
-
-(defgeneric move (ship target))
-
-(defmethod move ((ship ship) (target list))
-  (incf (x-coordinate ship) (car target))
-  (incf (y-coordinate ship) (cdr target)))
-
-(defmethod move ((ship ship) (target symbol))
-  (aif (gethash target +directions+)
-      (progn (setf (direction ship) target)
-	     (move ship it))
-    (error "bad direction: ~A~%" target)))
-
-(defmethod move ((ship string) target)
-  (aif (getship ship) 
-      (move it target)
-    (error "Ship ~A, doesn't exists" ship)))
-
-(defun get-ship-name (ship)
-  (block name-search
-    (maphash (lambda (key value)
-	       (when (eql value ship)
-		 (return-from name-search key)))
-	     *all-ships*)))
-
-(defgeneric peek (peeker))
-
-(defmethod peek ((peeker string))
-  (let ((ship-peeker (getship peeker)))
-    (maphash (lambda (name ship)
-	       (unless (equal name peeker)
-		 (remember name ship-peeker)
-		 (remember peeker ship)))
-	   *all-ships*)))
-
-(defmethod peek ((peeker ship))
-  (peek (get-ship-name peeker)))
-
-(defgeneric attack (attacker))
-
-(defmethod attack ((attacker ship))
-  (maphash (lambda (name ship)
-	     (when (in-vector (location attacker) (direction attacker) (location ship))
-	       (remove-ship name)))
-	   *all-ships*))
+(defun attack (attacking-ship)
+  (dolist (target (remove attacking-ship *all-ships*))
+    (if (in-vector (location attacking-ship) (direction attacking-ship) (location target))
+	(progn (setf (alive target) nil)
+	       (dolist (ship (remove target *all-ships*))
+		 (refresh-memory-by-name ship (name target))))
+	(refresh-memory-by-name target (name attacking-ship)))))
 
 (defgeneric print-world (ship version))
 (defmethod print-world (ship (version (eql :simple)))
@@ -209,27 +256,35 @@
     (setf (gethash (cons (+ 15 (x-coordinate ship))
 			 (- 15 (y-coordinate ship)))
 		   ships)
-	  (cons "@" 0))
-    (maphash (lambda (name lship)
-	       (setf (gethash (cons (+ 15 (x-coordinate lship))
-				    (- 15 (y-coordinate lship)))
-			      ships)
-		     (cons name (gethash name (ages (memory ship))))))
-	     (ships (memory ship)))
+	  ship)
+    (loop for ship in (rest (memory ship)) by #'cddr
+       do (setf (gethash (cons (+ 15 (x-coordinate ship))
+			       (- 15 (y-coordinate ship)))
+			 ships)
+		ship))
     (dotimes (y 30)
       (dotimes (x 30)
 	(aif (gethash (cons x y) ships)
-	    (case (cdr it)
-	      (0 (princ (string-upcase (subseq (car it) 0 1))))
-	      (1 (princ (string-downcase (subseq (car it) 0 1))))
-	      (otherwise (princ "~")))
-	  (princ "~")))
+	     (cond ((eql ship it)
+		    (princ "@"))
+		   ((not (alive (original it)))
+		    (princ "X"))
+		   (t
+		    (case (age it)
+		      (0 (princ "S"))
+		      (1 (princ "s"))
+		      (2 (princ "^"))
+		      (otherwise (princ "~")))))
+	     (princ "~")))
       (princ (string #\newline)))))
 
-(defun make-keyword (string)
-  (intern (string-upcase string) :keyword))
+(defclass human-ship (ship-with-memory) ())
+(defclass computer-ship (ship-with-memory) ())
 
-(defmethod turn (ship)
+(defgeneric turn (ship)
+  (:documentation "Governs the behavior of ship"))
+
+(defmethod turn ((ship human-ship))
   (print-world ship :simple)
   (format t "Action:~% Move)~% Peek)~% Attack)~% Quit)~%-: ")
   (case (make-keyword (read-line))
@@ -238,15 +293,76 @@
     (:peek (peek ship))
     (:attack (attack ship))
     (:quit (return-from turn nil)))
-  (age (memory ship))
+  (age-memory ship)
   t)
 
+(defmemoized get-location-probability-map (location time)
+  (let ((old-location-map (if (consp location)
+			      (let ((map (make-hash-table :test #'equal)))
+				(setf (gethash location map) 1)
+				map)
+			      location)))
+    (if (zerop time)
+	old-location-map
+	(let ((new-location-map (make-hash-table :test #'equal)))
+	  (maphash (lambda (location probability)
+		     (dolist (direction (get-directions))
+		       (setf/incf (gethash (move-location location direction) new-location-map)
+				  (/ probability 8))))
+		   old-location-map)
+	  (get-location-probability-map new-location-map (1- time))))))
+
+(defun chance-to-be-in-vector (start direction location-maps)
+  (let ((chance 0))
+    (dolist (map location-maps)
+      (let ((chance-for-map 0))
+	(maphash (lambda (location probability)
+		   (if (in-vector start direction location)
+		       (incf chance-for-map probability)))
+		 map)
+	    (incf chance (* chance-for-map (- 1 chance)))))
+    chance))
+
+(defun chance-to-hit (ship location-maps)
+  (chance-to-be-in-vector (location ship) (direction ship) location-maps))
+
+(defun chance-to-be-hit (ship location-maps)
+  (/
+   (apply #'+ (mapcar (lambda (direction)
+			(chance-to-be-in-vector (location ship) direction location-maps))
+		      (get-directions)))
+   8))
+  
+; peek - memory age threshold
+; attack - chance-to-hit threshold
+; move - chance-to-be-hit threshold
+
+(let ((age-threshold 2)
+      (cth-threshold 1/2))
+
+(defmethod turn ((ship computer-ship))
+  (let ((maps (map-ship-memory ship (lambda (ship) 
+				      (get-location-probability-map (location ship) (age ship))))))
+    (cond ((> (average (map-ship-memory ship #'age)) 1)
+	   (peek ship))
+	  ((> (chance-to-hit ship maps) 1/2)
+	   (attack ship))
+	  (t
+	   (move ship (greatest (shuffle-list (get-directions))
+				(lambda (x y)
+				  (< (chance-to-be-hit (move-location (location ship) x) maps)
+				     (chance-to-be-hit (move-location (location ship) y) maps)))))))
+    (age-memory ship)
+    t))
+	    
+)
+
 (defun main ()
-  (add-ship "first" '(1 . 1) :south)
-  (add-ship "second" '(-1 . -1) :north)
-  (initialize-memories *all-ships*)
-  (loop while (aand (getship "first")
+  (add-ship "player" '(1 . 1) :south 'human-ship)
+  (add-ship "computer" '(-1 . -1) :north 'computer-ship)
+  (loop while (aand (alive? (first *all-ships*))
 		    (turn it)
-		    (getship "second")
+		    (alive? (second *all-ships*))
 		    (turn it)))
-  (clrhash *all-ships*))
+  (print-world (get-ship-by-name "player") :simple)
+  (setf *all-ships* nil))
